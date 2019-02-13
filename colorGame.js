3b18e8c2f684cde2b1c1e7e77086162f6f89358d
Belinda Gower <belinda.gower@simplyhealth.co.uk>
2019-02-13 09:25:04 +0000

    generating random colours

--- colorGame.js
+++ colorGame.js
@@ -1,15 +1,24 @@
-var colors = [
-  "rgb(255, 100, 10)",
-  "rgb(150, 255, 0)",
-  "rgb(200, 30, 50)",
-  "rgb(147, 255, 203)",
-  "rgb(0, 0, 155)",
-  "rgb(137, 165, 87)"
-]
+var colors =  generateRandomColor(6);
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function() {
+  //generate all new colors
+  colors = generateRandomColor(6);
+  //pick new random color from array
+  pickedColor = pickColor();
+  //change colorDisplay to match picked color
+  colorDisplay.textContent = pickedColor;
+  //change colors of squares
+  for(var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+  }
+})
 
 // set the rgb code for the banner to be displayed in the heading
 colorDisplay.textContent = pickedColor;
@@ -24,10 +33,48 @@ for(var i = 0; i < squares.length; i++) {
     var clickedColor = this.style.backgroundColor;
     // compare colour to pickedColor
     if(clickedColor === pickedColor) {
-      alert("correct");
+      messageDisplay.textContent = "Correct!";
+      changeColors(clickedColor);
+      h1.style.backgroundColor = pickedColor;
     } else {
-      alert("wrong");
-      // clickedColor = this.style.backgroundColor("#232323");
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again";
     }
   });
+}
+
+function changeColors(color) {
+  //loop through all squares
+  for(var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
+  //change each color to match given color
+}
+
+function pickColor() {
+  //pick random number, math.floor makes it a whole number
+  var random = Math.floor(Math.random() * colors.length); //then multiply it my the number in the array
+  return colors[random];
+}
+
+function generateRandomColor(num) {
+  //make an array
+  var arr = []
+  //repeat num times
+  for(var i = 0; i < num; i++){
+    //get random color and push into array
+    arr.push(randomColor());
+  }
+  //return the array
+  return arr;
+}
+
+function randomColor() {
+  //pick a "red" from 0 - 255
+  var r = Math.floor(Math.random() * 256);
+  //pick a "green" from 0 - 255
+  var g = Math.floor(Math.random() * 256);
+  //pick a "blue" from 0 - 255
+  var b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
